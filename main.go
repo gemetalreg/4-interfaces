0bcb38422c7ee737c114c2edeb34b13ad49bf669
Andrei <itunereg@gmail.com>
2026-04-26 07:50:38 +0300

    struct

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
+import "time"
+
+type Bin struct {
+	id        string
+	private   bool
+	createdAt time.Time
+	name      string
+}
+
+type BinList struct {
+	Bins []Bin
+}
+
+func newBin(id string, private bool, createdAt time.Time, name string) *Bin {
+	return &Bin{id, private, createdAt, name}
+}
+
+func newBinList() *BinList {
+	return &BinList{
+		Bins: make([]Bin, 0), // инициализируем пустой срез
+	}
+}
+
 func main() {
 
 }
